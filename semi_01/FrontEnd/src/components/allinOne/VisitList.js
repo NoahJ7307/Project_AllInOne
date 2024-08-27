fea399792cf2e6ec993e8d678eeb43d6a81e637f
Noah7307 <allenkoelho@gmail.com>
2024-08-27 23:55:30 +0900

    H

--- semi_01/FrontEnd/src/components/allinOne/VisitList.js
+++ semi_01/FrontEnd/src/components/allinOne/VisitList.js
@@ -2,17 +2,79 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Box, Button, Grid, Modal, TextField, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
 
 const VisitList = () => {
+    const [formData, setFormData] = useState({
+        name_visit: '',
+        phone_visit: '',
+        date_visit: dayjs(),
+        dong_visit: 0,
+        ho_visit: 0,
+        carNumber_visit: '',
+    });
     const [users, setUsers] = useState([]);
     const [selected, setSelected] = useState([]);
     const [open, setOpen] = useState(false);
     const handleOpen = () => {
-        // console.log(selected);
-        // console.log(users.find(i => i.id === selected[0])?.name_visit||"");
-        selected.length == 1 ? setOpen(true) : alert("데이터 수정은 1개씩만 가능합니다")
+        if (selected.length !== 1) {
+            alert("데이터 수정은 1개씩만 가능합니다");
+            return;
+        }
+
+        const selectedUser = users.find(i => i.id === selected[0]);
+
+        if (selectedUser) {
+            setFormData({
+                name_visit: selectedUser.name_visit || '',
+                phone_visit: selectedUser.phone_visit || '',
+                date_visit: dayjs(selectedUser.date_visit) || dayjs(),
+                dong_visit: selectedUser.dong_visit || 0,
+                ho_visit: selectedUser.ho_visit || 0,
+                carNumber_visit: selectedUser.carNumber_visit || ''
+            });
+            setOpen(true);
+        } else {
+            alert("선택한 사용자가 없습니다.");
+        }
     };
     const handleClose = () => setOpen(false);
+
+    const handleInputChange = (e) => {
+        const { id, value } = e.target;
+        setFormData({ ...formData, [id]: value });
+    };
+
+    const handleDateChange = (newDate) => {
+        setFormData(prevFormData => ({ ...prevFormData, date_visit: newDate }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const apiUrl = '/api/visit';
+
+        axios.post(apiUrl, formData)
+            .then((response) => {
+                console.log('Form submitted successfully:', response.data);
+                alert("예약 완료");
+                setFormData({
+                    name_visit: '',
+                    phone_visit: '',
+                    date_visit: dayjs(),
+                    dong_visit: 0,
+                    ho_visit: 0,
+                    carNumber_visit: '',
+                });
+            })
+            .catch((error) => {
+                console.error('There was an error submitting the form:', error);
+                alert("예약 실패");
+            });
+    };
 
     useEffect(() => {
         axios.get('/api/visit')
@@ -21,6 +83,7 @@ const VisitList = () => {
                     id: data.id_visit,
                     date_visit: data.date_visit,
                     name_visit: data.name_visit,
+                    phone_visit: data.phone_visit,
                     carNumber_visit: data.carNumber_visit,
                     dong_visit: data.dong_visit,
                     ho_visit: data.ho_visit
@@ -35,10 +98,11 @@ const VisitList = () => {
 
     const columns = [
         { field: 'id', headerName: 'ID', width: 30 },
-        { field: 'date_visit', headerName: '방문일', width: 170 },
+        { field: 'date_visit', headerName: '방문일', width: 130 },
         { field: 'name_visit', headerName: '방문자 이름', width: 130 },
-        { field: 'carNumber_visit', headerName: '방문 차량번호', width: 130 },
-        { field: 'dong_visit', headerName: '동', type: 'number', width: 90 },
+        { field: 'phone_visit', headerName: '방문자 전화번호', width: 150 },
+        { field: 'carNumber_visit', headerName: '방문 차량번호', width: 110 },
+        { field: 'dong_visit', headerName: '동', type: 'number', width: 70 },
         { field: 'ho_visit', headerName: '호', type: 'number', width: 90 },
     ];
 
@@ -56,6 +120,7 @@ const VisitList = () => {
                     id: data.id_visit,
                     date_visit: data.date_visit,
                     name_visit: data.name_visit,
+                    phone_visit: data.phone_visit,
                     carNumber_visit: data.carNumber_visit,
                     dong_visit: data.dong_visit,
                     ho_visit: data.ho_visit
@@ -110,26 +175,86 @@ const VisitList = () => {
                     <Typography variant="h6" fontWeight={100} mb={2}>
                         방문 예약
                     </Typography>
-                    <Grid container spacing={2}>
+                    <Grid container spacing={3}>
                         <Grid item xs={12}>
                             <TextField
                                 required
                                 fullWidth
                                 id="name_visit"
                                 label="방문자 이름"
-                                value={users.find(i => i.id === selected[0])?.name_visit || ""}
-                                // onChange={handleInputChange}
+                                value={formData.name_visit}
+                                onChange={handleInputChange}
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="phone_visit"
+                                label="방문자 전화번호"
+                                value={formData.phone_visit}
+                                onChange={handleInputChange}
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="carNumber_visit"
+                                label="방문자 차량번호"
+                                value={formData.carNumber_visit}
+                                onChange={handleInputChange}
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="dong_visit"
+                                label="동"
+                                value={formData.dong_visit}
+                                onChange={handleInputChange}
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="ho_visit"
+                                label="호"
+                                value={formData.ho_visit}
+                                onChange={handleInputChange}
                                 variant="outlined"
                             />
                         </Grid>
                     </Grid>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12} sx={{ mt: 2 }}>
+                                <DemoContainer components={['DesktopDatePicker']}>
+                                    <DesktopDatePicker
+                                        required
+                                        fullWidth
+                                        id="date_visit"
+                                        label="방문 일자"
+                                        value={formData.date_visit}
+                                        onChange={handleDateChange}
+                                        renderInput={(params) => <TextField {...params} />} />
+                                </DemoContainer>
+                            </Grid>
+                        </Grid>
+                    </LocalizationProvider>
                     <Button type="submit" variant="contained" sx={{ mt: 2 }}
-                    // onClick={handleSubmit}
+                        onClick={handleSubmit}
                     >수정</Button>
                 </Box>
-            </Modal>
+            </Modal >
 
-        </Box>
+        </Box >
     );
 }
 
